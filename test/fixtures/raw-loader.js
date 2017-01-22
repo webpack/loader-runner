fb7f9835f60eacc9f955bb6c814069ac5ec3ed2b
Sean Larkin <sean.larkin@cuw.edu>
2017-01-22 16:56:59 -0600

    revert me accidentally pushing a branch

--- test/fixtures/raw-loader.js
+++ test/fixtures/raw-loader.js
@@ -1,5 +1,5 @@
 exports.__es6Module = true;
 exports.default = function(source) {
-	return new Buffer(source.toString("hex") + source.toString("utf-8"), "utf-8");
+	return Buffer.from(source.toString("hex") + source.toString("utf-8"), "utf-8");
 };
 exports.raw = true;
