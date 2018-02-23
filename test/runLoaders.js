25a29800d20cbd789542e070c29d6383090fedd4
Сковорода Никита Андреевич <chalkerx@gmail.com>
2018-02-23 15:07:04 +0300

    Avoid using deprecated Buffer constructor
    This raises the minimum Node.js version from 4.3.0 to 4.5.0,
    but that should be fine, as 4.5.0 was released very long ago
    and even the whole 4.x branch will be out of LTS support soon.
    
    Probably almost no one uses Node.js versions 4.3.x and 4.4.x nowdays.
    
    webpack/webpack states node": ">=6.11.5" requirement already.
    
    Refs:
    https://nodejs.org/api/deprecations.html#deprecations_dep0005_buffer_constructor
    https://github.com/webpack/webpack/pull/6356

--- test/runLoaders.js
+++ test/runLoaders.js
@@ -11,7 +11,7 @@ describe("runLoaders", function() {
 			resource: path.resolve(fixtures, "resource.bin")
 		}, function(err, result) {
 			if(err) return done(err);
-			result.result.should.be.eql([new Buffer("resource", "utf-8")]); // eslint-disable-line
+			result.result.should.be.eql([Buffer.from("resource", "utf-8")]);
 			result.cacheable.should.be.eql(true);
 			result.fileDependencies.should.be.eql([
 				path.resolve(fixtures, "resource.bin")
